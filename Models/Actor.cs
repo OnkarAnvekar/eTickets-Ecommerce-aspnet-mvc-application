f62f100a7513009f77e0ab652d2255a0db4badef
Onkar <oanvekar.oa@gmail.com>
2023-04-02 18:40:28 +0530

    Add project files.

---
+++ Models/Actor.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication6.Models
+{
+    public class Actor
+    {
+        [Key]
+        public int Id { get; set; }
+
+        p
+    }
+}
